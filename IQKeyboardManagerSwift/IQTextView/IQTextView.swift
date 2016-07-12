b9b129083cd65ef4beb4c2f1855440d28f2ba74d
Wynne M Lo <wynnemlo@gmail.com>
2016-07-12 15:55:58 +0800

    Made placeholder text public

--- IQKeyboardManagerSwift/IQTextView/IQTextView.swift
+++ IQKeyboardManagerSwift/IQTextView/IQTextView.swift
@@ -97,6 +97,19 @@ open class IQTextView : UITextView {
         }
     }
     
+    
+    open var placeholderColor: UIColor? {
+        get {
+            return placeholderLabel?.textColor
+        }
+        
+        set {
+            if placeholderLabel !== nil {
+                placeholderLabel.textColor = placeholderColor
+            }
+        }
+    }
+    
     override open var text: String! {
         
         didSet {
